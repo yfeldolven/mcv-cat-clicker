7abe1e0fed9b4f3c065ba9a9cd05ddc581c7e6cb
Youssef Khairy <youssefadamkhairy@gmail.com>
2018-04-19 08:43:06 +0200

    two Cats (used a function)

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
+'use strict';
 var pageNum = document.getElementById('num'),
-    catv = document.getElementById('v'),
-    num = 0 ,
-    img , numv;
+	catv = document.getElementById('v');
+
+function cat(src , name){
+	var num = 0 , img , numv , catNV;
+	img = document.createElement('img');
+	img.setAttribute('src','pic/'+src);
+	catv.appendChild(img);
+
+	numv = document.createElement('p');
+	numv.textContent = num ;
+	pageNum.appendChild(numv);
+
+	catNV = document.createElement('p');
+	catNV.textContent = name ;
+	pageNum.appendChild(catNV);
+
+	img.addEventListener('click',function(){
+		num ++ ;
+		numv.textContent = num ;
+	});
+}
 
 
-img = document.createElement('img');
-img.setAttribute('src','pic/pespes.jpg');
-catv.appendChild(img);
-
-numv = document.createElement('p');
-numv.textContent = num ;
-pageNum.appendChild(numv);
-
-
-img.addEventListener('click',function(){
-    num ++ ;
-    numv.textContent = num ;
-});
-
-
+cat('pespes.jpg','Pespes');
+cat('nemo.jpg','Nemo');
