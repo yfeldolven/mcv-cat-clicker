f346eaec75771bc6205f693dab88633fdb3bf868
Youssef Khairy <youssefadamkhairy@gmail.com>
2018-04-19 08:54:01 +0200

    fixed the view

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 'use strict';
-var pageNum = document.getElementById('num'),
-	catv = document.getElementById('v');
+var catV = document.getElementById('v');
 
 function cat(src , name){
-	var num = 0 , img , numv , catNV;
-	img = document.createElement('img');
-	img.setAttribute('src','pic/'+src);
-	catv.appendChild(img);
-
+    var num = 0 , img , numv , catNV;
+    
+    catNV = document.createElement('p');
+	catNV.textContent = name ;
+	catV.appendChild(catNV);
+    
 	numv = document.createElement('p');
 	numv.textContent = num ;
-	pageNum.appendChild(numv);
+	catV.appendChild(numv);
 
-	catNV = document.createElement('p');
-	catNV.textContent = name ;
-	pageNum.appendChild(catNV);
+	img = document.createElement('img');
+    img.setAttribute('src','pic/'+src);
+    img.style.width = '300px';
+	catV.appendChild(img);
 
 	img.addEventListener('click',function(){
 		num ++ ;
