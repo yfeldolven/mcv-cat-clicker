2b9b31fd9c13f65172962f9a26c6dc068ef28ef3
Youssef Khairy <youssefadamkhairy@gmail.com>
2018-04-19 16:42:53 +0200

    Many cats

--- app.js
+++ app.js
@@ -1,28 +1,52 @@
 'use strict';
-var catV = document.getElementById('v');
+var catV = document.getElementById('v'),
+	cats = ['foody','mshmsh','nemo','nyny','pespes','smarty','utut'],
+	catNum = [];
 
-function cat(src , name){
-    var num = 0 , img , numv , catNV;
-    
-    catNV = document.createElement('p');
-	catNV.textContent = name ;
-	catV.appendChild(catNV);
-    
-	numv = document.createElement('p');
-	numv.textContent = num ;
-	catV.appendChild(numv);
+for(var i=0 ; i<cats.length ; i++){
 
-	img = document.createElement('img');
-    img.setAttribute('src','pic/'+src);
-    img.style.width = '300px';
-	catV.appendChild(img);
+	var catList = document.createElement('span');
+	catList.textContent = cats[i] ;
+	catList.setAttribute('class','list');
+	catV.appendChild(catList);
 
-	img.addEventListener('click',function(){
-		num ++ ;
-		numv.textContent = num ;
-	});
+
+
+	catList.addEventListener('click', (function(i){
+
+		return function(){
+			var img , numv , catNV , div;
+			
+			div = document.createElement('div');
+        
+			catNV = document.createElement('p');
+			catNV.textContent = cats[i] ;
+			div.appendChild(catNV);
+
+			numv = document.createElement('p');
+			if(catNum[i]==null){catNum[i]=0;}
+			numv.textContent = catNum[i] ; 
+			div.appendChild(numv);
+
+			img = document.createElement('img');
+			img.setAttribute('src','pic/'+ cats[i] +'.jpg');
+			img.style.width = '300px';
+			div.appendChild(img);
+
+
+			if(catV.childElementCount>cats.length){
+				catV.replaceChild(div,catV.childNodes[cats.length]);
+			}else{
+				catV.appendChild(div);
+			}
+			
+			
+			img.addEventListener('click',function(){
+				catNum[i] ++ ;
+				numv.textContent = catNum[i] ;
+				
+			});
+		};
+	} (i) )
+	);
 }
-
-
-cat('pespes.jpg','Pespes');
-cat('nemo.jpg','Nemo');
